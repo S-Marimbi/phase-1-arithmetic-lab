3841182973612b0e9c37e0993660d0d99eb94c7e
S-Marimbi <marimbinjunge@gmail.com>
2024-03-24 15:58:29 +0300

    Completed Arithmetic_lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
 
-// Write your code here
+let num3 = 11;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(20);
+Math.max(20, 6, 12, 9, 11);
+console.log(max);
+
+
+
